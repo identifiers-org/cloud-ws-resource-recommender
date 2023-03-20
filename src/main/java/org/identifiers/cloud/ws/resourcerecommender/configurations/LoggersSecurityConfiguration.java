681d916dabf6f5925147765ae5eea6cced9062fc
Renato Juaçaba Neto <renatocjn@gmail.com>
2023-03-20 16:16:01 +0000

    Minor refactor

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/configurations/LoggersSecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/configurations/LoggersSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package org.identifiers.cloud.ws.resourcerecommender.configurations;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,25 +7,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import javax.annotation.PostConstruct;
-
 @Configuration
 @EnableWebSecurity
 public class LoggersSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${org.identifiers.cloud.ws.resourcerecommender.requiredrole}")
     String requiredRole; // Assumed that user gets role directly
 
-    String aExp;
-    @PostConstruct
-    public void setAccessExpression() {
-        aExp = String.format(
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       String aExp = String.format(
                 "isAuthenticated() and principal.claims['realm_access']['roles'].contains('%s')",
                 requiredRole
         );
-    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/actuator").access(aExp)
                 .antMatchers("/actuator/loggers/**").access(aExp)
