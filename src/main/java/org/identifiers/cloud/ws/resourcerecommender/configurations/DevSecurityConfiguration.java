6cc31b4f359ef138dcc1ef1dd32641083879e5ce
Renato Juaçaba Neto <renatocjn@gmail.com>
2023-07-04 11:19:11 +0100

    Adjusting test and profiles

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/configurations/DevSecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/configurations/DevSecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.identifiers.cloud.ws.resourcerecommender.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,27 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class LoggersSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Value("${org.identifiers.cloud.ws.resourcerecommender.requiredrole}")
-    String requiredRole; // Assumed that user gets role directly
+@Profile("development")
+public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       String aExp = String.format(
-                "isAuthenticated() and principal.claims['realm_access']['roles'].contains('%s')",
-                requiredRole
-        );
-
         http.authorizeRequests()
-                .antMatchers("/actuator").access(aExp)
-                .antMatchers("/actuator/loggers/**").access(aExp)
-                .antMatchers(HttpMethod.GET, "/actuator/health/**").permitAll()
-                .antMatchers(HttpMethod.POST,"/").permitAll()
+                .anyRequest().permitAll()
             .and()
                 .csrf().disable()
                 .logout().disable()
                 .formLogin().disable()
-                .anonymous().disable()
-                .oauth2ResourceServer().jwt();
+                .anonymous();
     }
 }
