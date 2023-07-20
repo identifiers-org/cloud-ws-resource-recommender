4c4613c9abec6bb3beb65b80339e48384a02a401
Renato Juaçaba Neto <renatocjn@gmail.com>
2023-07-20 09:44:29 +0100

    Cleanup unused imports

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/configurations/DevSecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/configurations/DevSecurityConfiguration.java
@@ -1,9 +1,7 @@
 package org.identifiers.cloud.ws.resourcerecommender.configurations;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
